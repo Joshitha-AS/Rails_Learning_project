b34cdd06ccc54ad3774eb5c8c1db41c5b2930590
Joshitha-AS <asjoshitha2312@gmail.com>
2025-04-17 12:06:41 +0530

    added roles column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_16_145803) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_17_063351) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -95,6 +95,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_16_145803) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
